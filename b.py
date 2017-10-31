c7394b667c07592356ed5836e6a1273b452b10c7
famishedrover <mudit.verma2014@gmail.com>
2017-10-31 22:38:44 +0530

    added comments

--- b.py
+++ b.py
@@ -1,25 +1,48 @@
 import cv2
+# opencv
 import numpy as np
+# numerical computations
 import matplotlib.pyplot as plt
-backsub = cv2.BackgroundSubtractorMOG() #background subtraction to isolate moving cars
-capture = cv2.VideoCapture("abbb.mp4") #change to destination on your pc 
+# plotting (at the end)
+
+# importing libraries
+
+backsub = cv2.BackgroundSubtractorMOG() 
+#background subtraction to isolate moving cars
+
+
+capture = cv2.VideoCapture("abbb.mp4") 
+# reading video
+
+
 i = 0
+# car_count
+
 minArea=1
+# min area for removing unnecessary moments
+
 kernel = np.ones((17,11),np.uint8)
-ff = 0 
+# kernel for smoothening 
+
+
+ff = 0
+#current frame number
+
 flag = False 
+
+
 last_x = 0
 last_y = 0
+
+# setting threshold value
 thx = 80
 thy = 90 
 
 all_pts  = []
 
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# fourcc = cv2.cv.CV_FOURCC(*'XVID')
-# out = cv2.VideoWriter('output.avi',fourcc, 20.0, (288,384))
 
 
+# begining to read video.
 try:
     while True:
         ret, frame = capture.read()
@@ -34,28 +57,41 @@ try:
 
         pts = np.array([[0,0],[0,384],[200,0]], np.int32)
         cv2.fillPoly(frame, [pts], 2)
-
+        # blacking out unnecessary video area.
 
 
         fgmask = backsub.apply(frame, None, 0.01)
+        # applying background subtraction - creating mask
+
 
         # res = cv2.bitwise_and(frame,frame, mask= fgmask)
         # cv2.imshow('rr',res)
 
 
         erode=cv2.erode(fgmask,None,iterations=3) 
+        # eroding noise.
         moments=cv2.moments(erode,True)   
+        # finding moments
 
         area=moments['m00'] 
+        # READ OPENCV DOCS - m00 gives area
         if moments['m00'] >=minArea:
             x=int(moments['m10']/moments['m00'])
             y=int (moments['m01']/moments['m00'])
+            # above fomula gives centroid
+
+
             print 'X',x,'Y',y
             cv2.circle(frame,(x,y), 5, (0,0,255), -1)
+            # centroid point by color RED
             cv2.rectangle(frame,(x-10,y-20),(x+10,y),(0,0,255),2)
+            # creating square around point
 
         
             try :
+                # incrementing car count only when there is an abrubt movement of the 
+                # centroid point between consecutive frames along x,y
+
                 # print 'X DIFF:',last_x - x,'Y DIFF:',last_y-y
                 print abs(abs(last_x)+abs(last_y)-abs(x)-abs(y))
 
@@ -72,6 +108,7 @@ try:
             last_x = x
             last_y = y
 
+            # finding mean color in the rectangle drawn above. (can be done via numpy as well)
             for k in range(-10,10):
                 for j in range(-20,0):
                     a ,aa , aaa= frame[k+x][j+y]
@@ -85,14 +122,21 @@ try:
             print (r,g,b)
             hsv = cv2.cvtColor(np.uint8([[[b,g,r]]]), cv2.COLOR_BGR2HSV)
             print hsv
-
+            # hsv values are better for finding colorRanges. since "Value" is seperate 
+            # from hue and saturation.
 
 
         cv2.putText(frame,'FRAME:'+ str(ff) + ' COUNT: %r' %i, (10,30), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 2)
+        # showing Text onto frame.
+
 
         # cv2.line(frame,(650,0),(650,500),(255,0,0),1)
 
-        cv2.imshow("Track", frame)        
+
+        # showing frame.
+        cv2.imshow("Track", frame)  
+
+
         # out.write(frame)
 
         # cv2.circle(fgmask,(x,y), 5, (0,0,255), -1)
@@ -106,6 +150,11 @@ except:
 
 
 cv2.destroyAllWindows()
+
+
+# plotting the changes in the position of the centroid , since the count increases 
+# when the changes are abrubt so , this can be verified by looking at the peaks 
+# in the plot.
 
 alpha = []
 beta = []
